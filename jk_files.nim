9865e10c432e16eb45cae2c6bc93407d720063b4
aaaaninja <keshigomu71505@gmail.com>
2019-08-19 17:40:32 +0900

    コレクション操作の実験

---
+++ jk_files.nim
@@ -0,0 +1,10 @@
+import os
+import algorithm,tables,hashes,math
+import sets,lists,critbits,sequtils,intsets,strutils
+
+const aaaa = [1,2,3,4]
+echo aaaa
+echo aaaa[3]
+
+for i in walkDirRec("."):
+  echo i
